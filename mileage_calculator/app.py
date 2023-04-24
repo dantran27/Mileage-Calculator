6296cbd5e8aa482ef41e17e865fa7c42fa5af036
dantran27 <trand567@hsd.k12.or.us>
2023-04-24 13:23:09 -0700

    worked on widgets

--- mileage_calculator/app.py
+++ mileage_calculator/app.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
                              QVBoxLayout,QHBoxLayout,QComboBox,
-                             QLabel,QLineEdit)
+                             QLabel,QLineEdit,QSpinBox,QPushButton)
 from PyQt6.QtGui import QPalette, QColor
 
 class MainWindow(QMainWindow):
@@ -9,13 +9,32 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
-        self.setWindowTitle("My App")
+        
+        self.setWindowTitle("MIlaeage Calculator")
         layout = QVBoxLayout()
+
+
 
         # set to main layout
         gui = QWidget()
+
         gui.setLayout(layout)
         self.setCentralWidget(gui)
+        
+        label = QLabel("Press here to calculate miles!")
+        font = label.font()
+        font.setPointSize(10)
+        label.setFont(font)
+        layout.addWidget(label)
+
+        self.setCentralWidget(gui)
+        
+        Calculate_button = QPushButton(text="Calculate", parent=self)
+        Calculate_button.setFixedSize(100, 60)
+
+        layout.addWidget(Calculate_button)
+
+
 
 
 app = QApplication(sys.argv)
--- tests/test_mileage_calculator.py
+++ tests/test_mileage_calculator.py
@@ -1,5 +1,32 @@
-from mileage_calculator import __version__
+import sys
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget,
+                             QVBoxLayout,QHBoxLayout,QComboBox,
+                             QLabel,QLineEdit,QSpinBox,QPushButton)
+from PyQt6.QtGui import QPalette, QColor
 
+class MainWindow(QMainWindow):
 
-def test_version():
-    assert __version__ == '0.1.0'
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.setWindowTitle("My App")
+        layout = QVBoxLayout()
+
+        # set to main layout
+        gui = QWidget()
+
+        gui.setLayout(layout)
+        self.setCentralWidget(gui)
+
+        
+        Calculate_button = QPushButton(text="Center", parent=self)
+        Calculate_button.setFixedSize(100, 60)
+
+        layout.addWidget(Calculate_button)
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
